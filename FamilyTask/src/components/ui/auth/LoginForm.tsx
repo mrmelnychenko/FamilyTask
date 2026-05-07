fadfddf21614875c28c019801a8e53486529a5bb
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2026-05-08 00:29:47 +0300

    change login forn and add react hook form and zod validate

--- FamilyTask/src/components/ui/auth/LoginForm.tsx
+++ FamilyTask/src/components/ui/auth/LoginForm.tsx
@@ -1,8 +1,8 @@
 import { supabase } from '@/src/lib/supabase';
-import { AUTH_ERRORS } from '@/src/utils/error';
+import { getAuthError } from '@/src/utils/error';
 import { Feather } from '@expo/vector-icons';
 import { router } from 'expo-router';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Pressable,
   View,
@@ -13,137 +13,138 @@ import { Button } from '../Button';
 import { Divider } from '../Divider';
 import { GoogleIcon } from '../../icons/GoogleIcon';
 import { AuthSwitchLink } from './AuthSwitchLink';
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginFormData, loginSchema } from '@/src/schemas/auth.schema';
 
 export function LoginForm() {
+  const [loading, setLoading] = useState(false);
+  const [generalError, setGeneralError] = useState<string | null>(null);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [emailError, setEmailError] = useState<string | null>(null);
-    const [passwordError, setPasswordError] = useState<string | null>(null);
-    const [generalError, setGeneralError] = useState<string | null>(null);
-  
-  
-    useEffect(() => {
-      setEmailError(null);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(data: LoginFormData) {
+    try {
       setGeneralError(null);
-    }, [email]);
-  
-    useEffect(() => {
-      setPasswordError(null);
-      setGeneralError(null);
-    }, [password]);
-  
-    async function handleLogin() {
-      let hasError = false;
-  
-      if (!email.trim()) {
-        setEmailError("Email is required");
-        hasError = true;
-      } else if (!/\S+@\S+\.\S+/.test(email)) {
-        setEmailError("Invalid email format");
-        hasError = true;
+      setLoading(true);
+
+      const { error } = await supabase.auth.signInWithPassword({
+        email: data.email.trim().toLowerCase(),
+        password: data.password,
+      });
+      if (error) {
+        setGeneralError(getAuthError(error.message));
       }
-  
-      if (!password) {
-        setPasswordError("Password is required");
-        hasError = true;
-      } else if (password.length < 6) {
-        setPasswordError("Password must be at least 6 characters");
-        hasError = true;
-      }
-  
-      if (hasError) return;
-  
-      try {
-        setLoading(true);
-        const { error: supabaseError } = await supabase.auth.signInWithPassword({
-          email: email.trim().toLowerCase(),
-          password,
-        });
-  
-        if (supabaseError) {
-          const message = AUTH_ERRORS[supabaseError.code || ''] || supabaseError.message || AUTH_ERRORS.default;
-  
-          if (supabaseError.code === 'invalid_credentials') {
-            setEmailError(" ");
-            setPasswordError(message);
-          } else {
-            setGeneralError(message);
-          }
-        }
-      } catch (err) {
-        setGeneralError("Server error. Please try again later");
-      } finally {
-        setLoading(false);
-      }
+
+    } catch {
+      setGeneralError("Server error. Please try again later");
+    } finally {
+      setLoading(false);
     }
+  }
 
-    return (
-        <View className="gap-4">
+  return (
+    <View className="gap-4">
 
-    <Input
-      label="Email"
-      error={emailError}
-      value={email}
-      placeholder="example@gmail.com"
-      onChangeText={setEmail}
-      autoCapitalize="none"
-      icon={(color) => <Feather name="mail" size={18} color={color} />}
-    />
+      {/* EMAIL */}
+      <Controller
+        control={control}
+        name="email"
+        render={({ field: { onChange, value } }) => (
+          <Input
+            label="Email"
+            value={value}
+            onChangeText={onChange}
+            placeholder="example@gmail.com"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            error={errors.email?.message}
+            icon={(color) => (
+              <Feather name="mail" size={18} color={color} />
+            )}
+          />
+        )}
+      />
 
-    <Input
-      label="Password"
-      error={passwordError}
-      value={password}
-      placeholder="••••••••"
-      onChangeText={setPassword}
-      secureTextEntry
-      icon={(color) => <Feather name="lock" size={18} color={color} />}
-    />
+      {/* PASSWORD */}
+      <Controller
+        control={control}
+        name="password"
+        render={({ field: { onChange, value } }) => (
+          <Input
+            label="Password"
+            value={value}
+            onChangeText={onChange}
+            placeholder="••••••••"
+            secureTextEntry
+            error={errors.password?.message}
+            icon={(color) => (
+              <Feather name="lock" size={18} color={color} />
+            )}
+          />
+        )}
+      />
 
-    <Pressable
-      className="self-end"
-      onPress={() => router.push("/")}
-    // onPress={() => router.push("/forgot-password")}
-    >
-      <Typo variant="h3" className="text-primary">
-        Забули пароль?
-      </Typo>
-    </Pressable>
-
-    {generalError && (
-      <View className="bg-red-50 p-3 rounded-xl border border-red-100">
-        <Typo className="text-danger text-center">{generalError}</Typo>
-      </View>
-    )}
-
-    <Button onPress={handleLogin} disabled={loading}>
-      <Typo variant="h3" className="text-white">
-        {loading ? "Завантаження..." : "Увійти"}
-      </Typo>
-    </Button>
-
-    <Divider />
-
-    <Button
-      className="bg-white border border-border"
-      onPress={() => console.log("google login")}
-    >
-      <View className="flex-row items-center gap-2">
-        <GoogleIcon width={18} height={18} />
-        <Typo variant="h3" className="text-text">
-          Увійти через Google
+      {/* FORGOT PASSWORD */}
+      <Pressable
+        className="self-end"
+        // onPress={() => router.push("/forgot-password")}
+        onPress={() => router.push("/")}
+      >
+        <Typo variant="label" className="text-primary">
+          Forgot password?
         </Typo>
-      </View>
-    </Button>
+      </Pressable>
 
-    <AuthSwitchLink
-      text="Ще немає акаунту?"
-      linkText="Зареєструватися"
-      href="/register"
-    />
+      {/* GENERAL ERROR */}
+      {generalError && (
+        <Typo className="text-danger text-center">
+          {generalError}
+        </Typo>
+      )}
 
-  </View>
-    )
+      {/* LOGIN BUTTON */}
+      <Button
+        onPress={handleSubmit(onSubmit)}
+        disabled={loading}
+      >
+        <Typo variant="h3" className="text-white">
+          {loading ? "Loading..." : "Увійти"}
+        </Typo>
+      </Button>
+
+      <Divider />
+
+      {/* GOOGLE */}
+      <Button
+        className="bg-white border border-border"
+        onPress={() => console.log("google")}
+      >
+        <View className="flex-row items-center gap-2">
+          <GoogleIcon width={18} height={18} />
+
+          <Typo variant="h3" className="text-text">
+            Continue with Google
+          </Typo>
+        </View>
+      </Button>
+
+      <AuthSwitchLink
+        text="Don't have an account?"
+        linkText="Sign up"
+        href="/register"
+      />
+
+    </View>
+  );
 }
